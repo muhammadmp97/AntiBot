f3912ed9ec92982f15c8057f764454bfc933655e
webpajooh <webpajooh@gmail.com>
2020-11-11 15:53:55 +0330

    Leave unknown chats

--- bot.php
+++ bot.php
@@ -1,4 +1,10 @@
 <?php
+use TeleBot\TeleBot;
 
 require './vendor/autoload.php';
 
+$tg = new TeleBot(BOT_TOKEN);
+
+if ($tg->chat->id != CHAT_ID) {
+    $tg->leaveChat(['chat_id' => $tg->chat->id]);
+}
