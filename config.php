14e465ea45b675d839b1c6dbac86887e0478398c
webpajooh <webpajooh@gmail.com>
2020-11-11 15:15:41 +0330

    Add configuration file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME', 'antibot');
+
+define('CHAT_ID', '');
+define('OWNER_ID', '');
