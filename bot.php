b7cc5dec26fa0c53d547622ee70902ddcaffa50c
webpajooh <webpajooh@gmail.com>
2020-11-12 11:44:40 +0330

    Add !unban and !unmute commands

--- bot.php
+++ bot.php
@@ -19,6 +19,15 @@ try {
         }
     });
 
+    $tg->listen('!unban', function () use ($tg) {
+        $user = $tg->message->reply_to_message->from;
+        $senderInfo = $tg->getChatMember(['chat_id' => CHAT_ID, 'user_id' => $tg->user->id]);
+
+        if (in_array($senderInfo->status, ['creator', 'administrator'])) {
+            $tg->unbanChatMember(['chat_id' => CHAT_ID, 'user_id' => $user->id]);
+        }
+    });
+
     $tg->listen('!mute', function () use ($tg) {
         $user = $tg->message->reply_to_message->from;
         $senderInfo = $tg->getChatMember(['chat_id' => CHAT_ID, 'user_id' => $tg->user->id]);
@@ -28,6 +37,19 @@ try {
                 'chat_id' => CHAT_ID,
                 'user_id' => $user->id,
                 'permissions' => '{}'
+            ]);
+        }
+    });
+
+    $tg->listen('!unmute', function () use ($tg) {
+        $user = $tg->message->reply_to_message->from;
+        $senderInfo = $tg->getChatMember(['chat_id' => CHAT_ID, 'user_id' => $tg->user->id]);
+
+        if (in_array($senderInfo->status, ['creator', 'administrator'])) {
+            $tg->restrictChatMember([
+                'chat_id' => CHAT_ID,
+                'user_id' => $user->id,
+                'permissions' => '{"can_send_messages": true, "can_send_media_messages": true, "can_send_polls": true, "can_send_other_messages": true, "can_invite_users": true}'
             ]);
         }
     });
